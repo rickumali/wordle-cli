72df7d41961fef6e2b2973325dc12e6ea8bfe924
Rick Umali <rickumali@gmail.com>
2023-02-24 21:32:15 -0500

    Adds initial pass of game loop
    % swift run
    [3/3] Build complete!
    wordle-cli by Rick Umali
    ? cured
    You entered: cured
    | C | U |-R-| E | D |
    ? walks
    You entered: walks
    | W |-A-| L | K | S |
    ? TRAMP
    You entered: TRAMP
    | T |+R+|-A-|+M+| P |
    ? aroma
    You entered: aroma
    |+A+|+R+|+O+|+M+|+A+|
    You got it in 4 guesses
    
    See TODOs for what's remaining

--- Sources/wordle-cli/main.swift
+++ Sources/wordle-cli/main.swift
@@ -1,1 +1,42 @@
-print("Hello, world!")
+print("wordle-cli by Rick Umali")
+
+/// TODO: Make this interact with dictionary
+func getCorrectWord() -> String {
+    return "aroma"
+}
+
+/// This variable holds the guess from the user
+var guess: String
+/// This variable tracks whether input is 5 letters or not
+var goodInput: Bool
+// This is two slahes BOO
+var correctWord: String = getCorrectWord()
+var guessRow: GuessRow
+for guessCounter in 1...6 {
+    goodInput = false
+    // TODO: Make this repeat/while a function
+    repeat {
+        // TODO: Make this announce the count
+        print("?", terminator: " ")
+        guess = readLine()!
+        print("You entered: \(guess)")
+        if guess.count == 5 {
+            goodInput = true
+        } else {
+            print("You need a five letter word!")
+        }
+    } while !goodInput
+    guessRow = GuessRow(guess: guess.uppercased())
+    // TODO: Check guess in dictionary
+    guessRow.updateRow(correctWord: correctWord.uppercased())
+    // TODO: Store previous GuessRows in array for redisplay?
+    print(guessRow.row())
+    if guessRow.matchesCorrectWord() {
+        // TODO: Make this a function. Cool message per value
+        print("You got it in \(guessCounter) guesses")
+        break
+    }
+    if guessCounter == 6 {
+        break
+    }
+}
