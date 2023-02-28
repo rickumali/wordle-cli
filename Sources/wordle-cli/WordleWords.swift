4a04a022fcbf93bbad67aedbe8ea2f0f11fc4dcd
Rick Umali <rickumali@gmail.com>
2023-02-27 21:31:59 -0500

    Adds WordleWords class load() function / Word list
    NOTE: This is from:
    
    https://forums.swift.org/t/in-search-of-recommendation-for-cocoapod-to-read-a-file-and-also-a-question-about-string-init-contentsoffile/19139
    
    NOTE: The word list is hardcoded to be in Projects/wordle-cli

--- Sources/wordle-cli/WordleWords.swift
+++ Sources/wordle-cli/WordleWords.swift
@@ -1,8 +1,21 @@
 //
 //  File.swift
-//  
 //
 //  Created by Rick Umali on 2/27/23.
 //
 
 import Foundation
+
+class WordleWords {
+    init() {
+    }
+
+    func load() throws {
+        let home = URL(fileURLWithPath: NSHomeDirectory())
+        let url = home.appendingPathComponent("Projects/wordle-cli/valid-wordle-words.txt")
+        let file = try String(contentsOf: url)
+        file.enumerateLines { (line, shouldContinue) in
+            print(line)
+        }
+    }
+}
