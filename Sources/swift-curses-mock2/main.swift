dda5f4006902792a90841f4b306cfc1dab3bb3ab
Rick Umali <rickumali@gmail.com>
2023-03-07 21:39:25 -0500

    Makes guess rows in UI
    I fixed the build error in 057430183 by wholesale
    deleting the wordle-cli/.build subdirectory and rebuilding.
    I think there was something leftover from previous work.

--- Sources/swift-curses-mock2/main.swift
+++ Sources/swift-curses-mock2/main.swift
@@ -5,3 +5,19 @@
 
 import Foundation
 import SwiftCursesTerm
+
+var term = SwiftCursesTerm()
+term.addStrTo(content: "wordle-cli (by Rick Umali)", line: 3, column: 3)
+term.addStrTo(content: "1: |   |   |   |   |   |", line: 5, column: 8)
+term.addStrTo(content: "2: |   |   |   |   |   |", line: 6, column: 8)
+term.addStrTo(content: "3: |   |   |   |   |   |", line: 7, column: 8)
+term.addStrTo(content: "4: |   |   |   |   |   |", line: 8, column: 8)
+term.addStrTo(content: "5: |   |   |   |   |   |", line: 9, column: 8)
+term.addStrTo(content: "6: |   |   |   |   |   |", line: 10, column: 8)
+term.addStrTo(content: "Q W E R T Y U I O P", line: 12, column: 10)
+term.addStrTo(content: "A S D F G H J K L", line: 13, column: 11)
+term.addStrTo(content: "Z X C V B N M", line: 14, column: 12)
+term.refresh()
+getch()
+term.shutdown()
+exit(EXIT_SUCCESS)
