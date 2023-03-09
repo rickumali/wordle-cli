8baef407e1d393d391ee57dc0c838455f2e55c26
Rick Umali <rickumali@gmail.com>
2023-03-08 21:16:00 -0500

    Adds prompt loop to mock2
    Remaining puzzle is where getch() and getstr() are coming from.
    I'm guessing it's the interoperability.

--- Sources/swift-curses-mock2/main.swift
+++ Sources/swift-curses-mock2/main.swift
@@ -6,6 +6,8 @@
 import Foundation
 import SwiftCursesTerm
 
+let prompt_len = 15
+
 var term = SwiftCursesTerm()
 term.addStrTo(content: "wordle-cli (by Rick Umali)", line: 3, column: 3)
 term.addStrTo(content: "1: |   |   |   |   |   |", line: 5, column: 8)
@@ -17,6 +19,27 @@ term.addStrTo(content: "6: |   |   |   |   |   |", line: 10, column: 8)
 term.addStrTo(content: "Q W E R T Y U I O P", line: 12, column: 10)
 term.addStrTo(content: "A S D F G H J K L", line: 13, column: 11)
 term.addStrTo(content: "Z X C V B N M", line: 14, column: 12)
+
+var guess_count: Int = 1
+while guess_count != 7 {
+    term.addStrTo(content: "Guess (\(guess_count) of 6)", line: 16, column: 3)
+    term.addStrTo(content: "         ", line: 16, column: 3 + prompt_len );
+    term.move(window: nil, line: 16, column: 3 + prompt_len)
+
+    term.refresh()
+    // KEY: This declares an empty array of 40 NULLs
+    var raw_str: [CChar] = Array(repeating: CChar(0), count: 40)
+    // KEY: Getting the 'address' by ampersand
+    // KEY: This was lucky, and Xcode guided me somewhat
+    getstr(&raw_str)
+    let guessString = String(cString: raw_str)
+    term.addStrTo(content: "Entered: \(guessString)", line: 17, column: 3);
+
+    // updateGuessRowGuiWide(g: guess_count, s: guessString)
+    guess_count += 1
+}
+term.addStrTo(content: "That's it! Any key to quit!", line: 17, column: 3);
+
 term.refresh()
 getch()
 term.shutdown()
