ae83b9b428a8b6239371ef77ce3849504bd83d98
Rick Umali <rickumali@gmail.com>
2023-03-10 21:30:35 -0500

    Adds a SwiftCursesTerm window (for guess board)
    This gets rid of magic numbers in source code

--- Sources/wordle-tui/main.swift
+++ Sources/wordle-tui/main.swift
@@ -43,14 +43,15 @@ func updateGuessRowGuiWide(g: Int, s: String) {
 }
 
 var game_title = term.newWindow(height: 1, width: 40, line: 3, column: 3)
+var guessesBoard = term.newWindow(height: 6, width: 40, line: 5, column: 8)
 
 term.addStr(window: game_title, content: "wordle-cli (by Rick Umali)", refresh: true)
-term.addStrTo(content: "1: |   |   |   |   |   |", line: 5, column: 8)
-term.addStrTo(content: "2: |   |   |   |   |   |", line: 6, column: 8)
-term.addStrTo(content: "3: |   |   |   |   |   |", line: 7, column: 8)
-term.addStrTo(content: "4: |   |   |   |   |   |", line: 8, column: 8)
-term.addStrTo(content: "5: |   |   |   |   |   |", line: 9, column: 8)
-term.addStrTo(content: "6: |   |   |   |   |   |", line: 10, column: 8)
+term.addStrTo(window: guessesBoard, content: "1: |   |   |   |   |   |", line: 0, column: 0)
+term.addStrTo(window: guessesBoard, content: "2: |   |   |   |   |   |", line: 1, column: 0)
+term.addStrTo(window: guessesBoard, content: "3: |   |   |   |   |   |", line: 2, column: 0)
+term.addStrTo(window: guessesBoard, content: "4: |   |   |   |   |   |", line: 3, column: 0)
+term.addStrTo(window: guessesBoard, content: "5: |   |   |   |   |   |", line: 4, column: 0)
+term.addStrTo(window: guessesBoard, content: "6: |   |   |   |   |   |", line: 5, column: 0, refresh: true)
 term.addStrTo(content: "Q W E R T Y U I O P", line: 12, column: 10)
 term.addStrTo(content: "A S D F G H J K L", line: 13, column: 11)
 term.addStrTo(content: "Z X C V B N M", line: 14, column: 12)
