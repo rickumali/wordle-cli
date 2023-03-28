62d4d9ecc6dcd1f8c9fbcc305418d2c0f33f4a82
Rick Umali <rickumali@gmail.com>
2023-03-27 20:00:30 -0400

    Creates WordleGameModel
    Placeholder functions initially

---
+++ Sources/wordle/WordleGameModel.swift
@@ -0,0 +1,29 @@
+//
+//  WordleGameModel.swift
+//
+//  Created by Rick Umali on 3/27/23.
+
+import Foundation
+
+public class WordleGameModel {
+    var guesses: [GuessRow]
+    var correctWord: String
+    var usedLetters: [String]
+
+    public init() {
+
+    }
+
+    public isFinished() -> Bool {
+        
+    }
+
+    public addNewGuess(g: GuessRow) {
+
+    }
+
+    public endMessage() -> String {
+
+    }
+
+}
