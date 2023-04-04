d949a5cfd9fd8345d62bc3f120e11b6440aa00e7
Rick Umali <rickumali@gmail.com>
2023-04-04 19:56:23 -0400

    Removes unnecessary parentheses

--- Sources/wordle-cli/main.swift
+++ Sources/wordle-cli/main.swift
@@ -56,7 +56,7 @@ while !game.isFinished() {
 if (game.isWon()) {
     // TODO: Add fancy message here
     // corectWordMessage(guessCount: guessCounter)
-    gameView.close(s: "You got it in \(game.guessCount) guess\((game.guessCount) > 1 ? "es" : "")!")
+    gameView.close(s: "You got it in \(game.guessCount) guess\(game.guessCount > 1 ? "es" : "")!")
 } else {
     gameView.close(s: "The correct word was \(correctWord.uppercased())")
 }
