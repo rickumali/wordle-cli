d054a9fa75f57caee56434488769091f8c5c8456
Rick Umali <rickumali@gmail.com>
2023-04-03 21:03:07 -0400

    Exercises read-only properties
    Really fussed with guessCount versus promptGuessCount
    
    "It was doing double-duty"

--- Tests/wordle-cliTests/WordleGameModelTests.swift
+++ Tests/wordle-cliTests/WordleGameModelTests.swift
@@ -30,22 +30,56 @@ class WordleGameModelTests: XCTestCase {
         XCTAssertFalse(game.isFinished())
         XCTAssertFalse(game.isWon())
         game.addNewGuess("cured")
-        XCTAssertEqual(game.guesses.last!.row(), "| C | U |-R-| E | D |")
+        XCTAssertEqual(game.guessesAry.last!.row(), "| C | U |-R-| E | D |")
         XCTAssertEqual(game.guesses.count, 1)
         XCTAssertFalse(game.isFinished())
         XCTAssertFalse(game.isWon())
         game.addNewGuess("walks")
-        XCTAssertEqual(game.guesses.last!.row(), "| W |-A-| L | K | S |")
+        XCTAssertEqual(game.guessesAry.last!.row(), "| W |-A-| L | K | S |")
         XCTAssertEqual(game.guesses.count, 2)
         XCTAssertFalse(game.isFinished())
         XCTAssertFalse(game.isWon())
         game.addNewGuess("tramp")
-        XCTAssertEqual(game.guesses.last!.row(), "| T |+R+|-A-|+M+| P |")
+        XCTAssertEqual(game.guessesAry.last!.row(), "| T |+R+|-A-|+M+| P |")
         XCTAssertEqual(game.guesses.count, 3)
         game.addNewGuess("aroma")
-        XCTAssertEqual(game.guesses.last!.row(), "|+A+|+R+|+O+|+M+|+A+|")
+        XCTAssertEqual(game.guessesAry.last!.row(), "|+A+|+R+|+O+|+M+|+A+|")
         XCTAssertEqual(game.guesses.count, 4)
+        XCTAssertEqual(game.guessCount, 4)
+        XCTAssertEqual(game.promptGuessCount, 5)
         XCTAssertTrue(game.isFinished())
         XCTAssertTrue(game.isWon())
+    }
+
+    func testCrazeGame() {
+        let game = WordleGameModel(correctWord: "craze")
+        XCTAssertFalse(game.isFinished())
+        XCTAssertFalse(game.isWon())
+        XCTAssertEqual(game.promptGuessCount, 1)
+        game.addNewGuess("grail") // Guess 1
+        XCTAssertEqual(game.guesses.last!.row(), "| G |+R+|+A+| I | L |")
+        XCTAssertFalse(game.isFinished())
+        XCTAssertFalse(game.isWon())
+        XCTAssertEqual(game.promptGuessCount, 2)
+        game.addNewGuess("track") // Guess 2
+        XCTAssertEqual(game.guesses.last!.row(), "| T |+R+|+A+|-C-| K |")
+        XCTAssertFalse(game.isFinished())
+        XCTAssertFalse(game.isWon())
+        XCTAssertEqual(game.promptGuessCount, 3)
+        game.addNewGuess("cramp") // Guess 3
+        XCTAssertEqual(game.guesses.last!.row(), "|+C+|+R+|+A+| M | P |")
+        XCTAssertEqual(game.promptGuessCount, 4)
+        game.addNewGuess("crabs") // Guess 4
+        XCTAssertEqual(game.guesses.last!.row(), "|+C+|+R+|+A+| B | S |")
+        XCTAssertEqual(game.promptGuessCount, 5)
+        game.addNewGuess("crazy") // Guess 5
+        XCTAssertEqual(game.guesses.last!.row(), "|+C+|+R+|+A+|+Z+| Y |")
+        XCTAssertEqual(game.promptGuessCount, 6)
+        game.addNewGuess("craze") // Guess 6 Last guess!
+        XCTAssertEqual(game.guesses.last!.row(), "|+C+|+R+|+A+|+Z+|+E+|")
+        XCTAssertTrue(game.isFinished())
+        XCTAssertTrue(game.isWon())
+        XCTAssertEqual(game.promptGuessCount, 7)
+        XCTAssertEqual(game.guessCount, 6)
     }
 }
